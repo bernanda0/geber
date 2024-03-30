dfe1a2da679c0d38cead59e8b15804a965f960a6
bernanda-rai <inotbnri@gmail.com>
2024-03-30 20:16:59 +0700

    Initial Commit

---
+++ geber/geberApp.swift
@@ -0,0 +1,17 @@
+//
+//  geberApp.swift
+//  geber
+//
+//  Created by bernanda on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct geberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
